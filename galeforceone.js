226e6850d45244cad2675f345766e566bbb576f6
Gale Van Rossem <galeforcevr@gmail.com>
2015-01-04 21:05:19 -0600

    added existing site files and images

---
+++ galeforceone.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+//$(document).ready(function() {
+  //  $("submit").click(function(){
+	//	$('.login').fadeOut(1000);
+	//});
+//});
+
+//$(document).ready(function(e) {
+  //  alert ("The page just loaded!");
+//});
+
+
+
+/*$(document).ready(function() {
+    $('.submit').click(function(){
+		$('.signin').hide(1000);
+	});
+});*/
+
+$(document).ready(function() {
+    $('.submit').click(function(){
+		$('.signin').fadeToggle(1000);
+	});
+});
+
